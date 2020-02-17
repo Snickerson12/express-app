ac460af4925aea88bc5c2a208127d0b5a0f2c931
Snickerson12 <laura.nickerson@gmail.com>
2020-02-17 13:26:01 -0600

    middleware static function built into express to access stylesheets

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ let app = express();
 
 app.set('view engine', 'ejs');
 
+// middleware for our static asset files
+// app.use('/assets', function(request, response, next){
+//     console.log(request.url)
+//     next();
+// });
+
+// express has a built in function to do all of the above for static files
+app.use('/assets', express.static('assets'));
+
 app.get('/', function(request, response){
     response.render('index');
 });
