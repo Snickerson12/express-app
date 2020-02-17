4c43da81c1d3470b01e4c04ba5272d7719aef357
Snickerson12 <laura.nickerson@gmail.com>
2020-02-17 12:30:22 -0600

    dynamic route params

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ let app = express();
 
 // set up get request routes 
 app.get('/', function(request, response){
-    response.send('this is the homepage')
+    response.send('this is the homepage');
 });
 
 app.get('/contact', function(request, response){
-    response.send('this is the contact page')
+    response.send('this is the contact page');
 });
+
+// dynamic url parameters
+app.get('/profile/:id', function(request, response){
+    response.send('You requested to see a profile with the ID of ' + request.params.id);
+})
+
+app.get('/profile/:name', function(request, response){
+    response.send('You requested to see a profile with the name of ' + request.params.id);
+})
 
 // listens to the port
 app.listen(3000);
